9793a43acb883f060167bf776c688480812b2f3b
mayanktap <mayanktaparia1992@gmail.com>
2023-03-24 10:12:34 +0530

    Fix issue failing changes in dev branch.

--- src/components/business.jsx
+++ src/components/business.jsx
@@ -1,9 +1,7 @@
-import {
-  BusinessStart,
-  ThumbnailSlider,
-  ProductLists,
-  ProductFeatures,
-} from '../pages/business';
+import ThumbnailSlider from './../pages/business/ThumbnailSlider';
+import BusinessStart from './../pages/business/BusinessStart';
+import ProductLists from './../pages/business/ProductLists';
+import ProductFeatures from './../pages/business/ProductFeatures';
 
 const Business = () =>  (
   <section>
---
+++ src/pages/business/Index.jsx
@@ -0,0 +1,11 @@
+import ThumbnailSlider from './ThumbnailSlider';
+import BusinessStart from './BusinessStart';
+import ProductLists from './ProductLists';
+import ProductFeatures from './ProductFeatures';
+
+export default {
+  ThumbnailSlider,
+  BusinessStart,
+  ProductLists,
+  ProductFeatures,
+}
