4a6b2506bd81cb570ea13708e255062eb514e8b2
mayanktap <mayanktaparia1992@gmail.com>
2023-05-24 20:28:33 +0530

    Update Login & Signup UI and integrated the Auth API. Added account confirmation page as well.

--- src/pages/user_authentication/Login.jsx
+++ src/pages/user_authentication/Login.jsx
@@ -1,20 +1,47 @@
+import { Auth } from 'aws-amplify';
+import { useRef } from 'react';
 import './stylesheets/login.css';
 
 const Login = () => {
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+
+  async function signIn() {
+    try {
+      const user = await Auth.signIn(emailInput.current.value, passwordInput.current.value);
+      console.log(user);
+      window.location.replace(window.location.origin + '/');
+    } catch (error) {
+      console.log('error signing in', error);
+    }
+  }
+
   return (
     <div>
       <div className="email-wrapper">
         <label className="email-label" htmlFor="email">Email</label>
-        <input className="email-input" type="text" id="email" name="email" />
+        <input
+          ref={emailInput}
+          className="email-input"
+          type="text"
+          id="email"
+          name="email" />
       </div>
 
       <div className="password-wrapper">
         <label className="password-label" htmlFor="password">Password</label>
-        <input className="password-input" type="text" id="password" name="password" />
+        <input
+          ref={passwordInput}
+          className="password-input"
+          type="text"
+          id="password"
+          name="password" />
       </div>
 
       <div className="login-btn-wrapper  u-pad-bottom">
-        <button className='login-btn'>Login</button>
+        <button
+          onClick={signIn}
+          className='login-btn'>Login</button>
       </div>
     </div>
   );
--- src/pages/user_authentication/Signup.jsx
+++ src/pages/user_authentication/Signup.jsx
@@ -1,28 +1,117 @@
 import './stylesheets/login.css';
+import { Auth } from 'aws-amplify';
+import { useRef, useState } from 'react';
 
 const Signup = () => {
-  return (
-    <div>
-      <div className="email-wrapper">
-        <label className="email-label" htmlFor="username">Email</label>
-        <input className="email-input" type="text" id="email" name="email" />
-      </div>
+  const [showConfirmationCode, setShowConfirmationCode] = useState(false);
+  const emailInput = useRef(null);
+  const passwordInput = useRef(null);
+  const confirmPasswordInput = useRef(null);
+  const confirmationemailInput = useRef(null);
+  const confirmationCodeInput = useRef(null);
 
-      <div className="password-wrapper">
-        <label className="password-label" htmlFor="password">Password</label>
-        <input className="password-input" type="text" id="password" name="password" />
-      </div>
+  async function signupClick() {
+    try {
+      if (passwordInput.current.value === confirmPasswordInput.current.value) {
+        const { user } = await Auth.signUp({
+          username: emailInput.current.value,
+          password: passwordInput.current.value,
+          attributes: {
+            email: emailInput.current.value,
+          },
+          autoSignIn: { // optional - enables auto sign in after user is confirmed
+            enabled: true,
+          },
+        });
+        console.log(user);
+        setShowConfirmationCode(true);
+      }
+    } catch (error) {
+      console.log('error signing up:', error);
+    }
+  }
 
-      <div className="confirm-password-wrapper">
-        <label className="confirm-password-label" htmlFor="confirmpassword">Confirm Password</label>
-        <input className="confirm-password-input" type="text" id="confirmpassword" name="confirmpassword" />
-      </div>
+  async function confirmSignUp() {
+    try {
+      await Auth.confirmSignUp(confirmationemailInput.current.value, confirmationCodeInput.current.value);
+      window.location.replace(window.location.origin + '/login');
+    } catch (error) {
+      console.log('error confirming sign up', error);
+    }
+  }
 
-      <div className="signup-btn-wrapper  u-pad-bottom">
-        <button className='signup-btn'>Sign Up</button>
+  if (showConfirmationCode) {
+    return (
+      <div>
+        <h3 className='account-confirmation'>Account Confirmation</h3>
+        <div className="confirmation-email-wrapper">
+          <label className="confirmation-email-label" htmlFor="confirmationemail">Email</label>
+          <input
+            ref={confirmationemailInput}
+            className="confirmation-email-input"
+            type="text"
+            id="confirmationemail"
+            name="confirmationemail" />
+        </div>
+  
+        <div className="code-wrapper">
+          <label className="code-label" htmlFor="confirmationcode">code</label>
+          <input
+            ref={confirmationCodeInput}
+            className="code-input"
+            type="text"
+            id="confirmationcode"
+            name="confirmationcode" />
+        </div>
+
+        <div className="code-btn-wrapper  u-pad-bottom">
+          <button
+            onClick={confirmSignUp}
+            className='submit-code-btn'>Submit</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div className="email-wrapper">
+          <label className="email-label" htmlFor="signupemail">Email</label>
+          <input
+            ref={emailInput}
+            className="email-input"
+            type="text"
+            id="signupemail"
+            name="signupemail" />
+        </div>
+  
+        <div className="password-wrapper">
+          <label className="password-label" htmlFor="signuppassword">Password</label>
+          <input
+            ref={passwordInput}
+            className="password-input"
+            type="text"
+            id="signuppassword"
+            name="signuppassword" />
+        </div>
+  
+        <div className="confirm-password-wrapper">
+          <label className="confirm-password-label" htmlFor="signupconfirmpassword">Confirm Password</label>
+          <input
+            ref={confirmPasswordInput}
+            className="confirm-password-input" 
+            type="text"
+            id="signupconfirmpassword"
+            name="signupconfirmpassword" />
+        </div>
+  
+        <div className="signup-btn-wrapper  u-pad-bottom">
+          <button
+            className='signup-btn'
+            onClick={signupClick}>Sign Up</button>
+        </div>
+      </div>
+    );
+  }
 };
   
 export default Signup;
