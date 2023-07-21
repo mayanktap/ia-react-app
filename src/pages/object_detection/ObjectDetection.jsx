318d01760f9af792c810c774509ebfee3bb1540b
mayanktap <mayanktaparia1992@gmail.com>
2023-07-21 15:19:26 +0530

    Add signurl for s3 object to access private s3 object and implement image modal.

--- src/pages/object_detection/ObjectDetection.jsx
+++ src/pages/object_detection/ObjectDetection.jsx
@@ -1,8 +1,34 @@
 import '@aws-amplify/ui-react/styles.css';
 import PropTypes from 'prop-types';
 import './object_detection.css';
+import { API, Auth } from 'aws-amplify';
+import ImageModal from './../modal/ImageModal';
+import { useState } from 'react';
 
 function ObjectDetection({ data }) {
+  const [s3Url, setS3Url] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const closeModalHandler = () => {
+    setShowModal(false);
+  };
+
+  async function viewS3Image(s3Url) {
+    let user = await Auth.currentAuthenticatedUser();
+    console.log(`user: ${JSON.stringify(user)} and s3Url: ${s3Url}`);
+
+    if(user['username']) {
+      API.get('useruploadedmediainfo', `/object-detection-url/${s3Url.replace('https://', '')}`, {
+        headers: { Authorization: user.signInUserSession.idToken.jwtToken },
+      }).then((response) => {
+        console.log(`response from object detection url: ${JSON.stringify(response)}`);
+        setShowModal(true);
+        setS3Url(response['signedUrl']);
+      }).catch((error) => {
+        console.log(JSON.stringify(error));
+      });
+    }
+  }
+
   if (data) {
     const fields = [
       'Email', 'S3 url', 'S3 etag', 'Object Type', 'Bounding Box X',
@@ -13,6 +39,7 @@ function ObjectDetection({ data }) {
 
     return (
       <div className='table-container text-white'>
+        { showModal && <ImageModal closeModalFunc={closeModalHandler} data={s3Url} /> }
         <table className="table">
           <thead>
             <tr>
@@ -26,7 +53,9 @@ function ObjectDetection({ data }) {
               <tr key={index}>
                 <td className='td'>{item['email']}</td>
                 <td className='td'>
-                  <img src={item['s3_url']} width="100" height="100" />
+                  <button className='view-image' onClick={() => viewS3Image(item['s3_url'])}>
+                    View Image
+                  </button>
                 </td>
                 <td className='td'>{item['s3_etag']}</td>
                 <td className='td'>{item['object_type']}</td>
